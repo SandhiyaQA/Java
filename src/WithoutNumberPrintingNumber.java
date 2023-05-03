0025538e3ca8e8815e687a4c74e43d90c0bced20
Dell <sandhiyaravi01@gmail.com>
2023-05-03 23:09:54 +0530

    practise code commi1

---
+++ src/WithoutNumberPrintingNumber.java
@@ -0,0 +1,5 @@
+public class WithoutNumberPrintingNumber {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/sample.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class sample {
+    public static void main(String[] args) {
+        String str="hello";
+       for(String s : str.split(""))
+           System.out.println(s);
+    }
+}
