4a0868b65dfa60bc751be035c691821ccc4d7e68
Dell <sandhiyaravi01@gmail.com>
2023-05-04 13:15:26 +0530

    array c1

---
+++ src/Area.java
@@ -0,0 +1,8 @@
+class Area
+{
+ static float i;
+    public static void main(String args[]) {
+        System.out.print("Hello,\nworld!"+i);
+
+    }
+}
---
+++ src/tets.java
@@ -0,0 +1,18 @@
+import java.sql.Array;
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
+public class tets {
+    public static void main(String[] args) {
+       String[] s={"red","blue"};
+       int[][] a={{2,2},{2, },{ 0,2}};
+        System.out.println(Arrays.deepToString(s));
+        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.deepToString(a));
+        System.out.println(Arrays.toString(a));
+        for(String j:s)
+        {
+        System.out.println(j);
+
+        }}}
+
